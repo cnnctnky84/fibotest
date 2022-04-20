7bad1bb76de35b46c10b4a1e88e86f036e1273cf
NiyaziAki <niyazi.aki@outlook.com>
2022-04-20 13:46:31 +0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace FİBONACCİ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int bir = 0, iki = 1, uc;
+            int kacincieleman;
+            Console.WriteLine("eleman say giriniz:");
+            kacincieleman = int.Parse(Console.ReadLine());
+
+
+            if (kacincieleman > 0)
+            {
+                if (kacincieleman == 1)
+                {
+                    Console.WriteLine(bir);
+                }
+                else
+                {
+
+
+                    for (int i = 0; i < kacincieleman - 2; i++)
+                    {
+
+                        uc = bir + iki;
+                        bir = iki;
+                        iki = uc;
+                        Console.WriteLine(uc);
+
+                    }
+
+                    bir = 0;
+                    iki = 1;
+
+                }
+            }
+
+
+
+            else
+            {
+
+                Console.WriteLine("o dan büyük deger giriniz.");
+
+            }
+        }
+    }
+}
